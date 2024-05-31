57fcf73742ef0715bc4c62a61bcff9e765639e83
RAKIB <rakibmahmudmridha@gmail.com>
2024-05-31 06:52:46 +0600

    crate and run server

---
+++ api/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = process.env.PORT || 5050;
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookie parser
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
